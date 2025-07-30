c2a21f972b57773b18d77f1acab6dd19b4ca2299
Md-Shakil-24 <shakilahmed14068682@gmail.com>
2025-07-30 07:10:57 +0600

    ok all

--- src/pages/PublisherCard.jsx
+++ src/pages/PublisherCard.jsx
@@ -1,9 +1,18 @@
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const PublisherCard = ({ publisher }) => {
   return (
     <Link to={`/all-articles?publisher=${encodeURIComponent(publisher.name)}`}>
       <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow">
+
+ <Helmet>
+              <title>PublisherCards | NewsHub</title>
+              <meta name="description" content="Learn more about MyApp and what we do." />
+              <meta property="og:title" content="About Us - MyApp" />
+            </Helmet>
+
+
         <figure className="px-10 pt-10">
           <img
             src={publisher.logo}
